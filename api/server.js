bbde7ba3ffc327441e18e005a789ede64490dc8f
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2020-01-12 05:11:13 -0600

    Completed MVP Found Bug. sqlite3 folder somehow was saved outside of data so it was not even pullin any data through the tables.

--- api/server.js
+++ api/server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-
-
 const helmet = require("helmet");
 
 const projectsRouter = require("../Router/projectRouter.js");
@@ -11,5 +9,9 @@ server.use(helmet());
 server.use(express.json());
 
 server.use("/api/projects", projectsRouter);
+
+server.get('/', (req, res) => {
+    res.send("You might actually pass Back-end, keep testing!!");
+})
 
 module.exports = server;
